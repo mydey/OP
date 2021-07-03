7124bc7313465d4233f905b5bc2211ca6c97c5de
tsoo <mydey@users.noreply.github.com>
2021-07-03 23:07:38 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+## 在线编译自用路由器固件，请勿操作，后果自负
+
+
+## 感谢
+- [OpenWrt](https://github.com/openwrt/openwrt)
+- [Lean's OpenWrt](https://github.com/coolsnowwolf/lede)
+
+
+## License
+[MIT](https://github.com/P3TERX/Actions-OpenWrt/blob/master/LICENSE) 
---
+++ diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+
+# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' ./target/linux/ipq40xx/Makefile
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
