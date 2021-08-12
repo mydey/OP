989d56a8bb9ef764919f18714e7f80d9cc8e34fc
tsoo <mydey@users.noreply.github.com>
2021-08-12 21:19:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #cd package
 #git clone https://github.com/destan19/OpenAppFilter.git
